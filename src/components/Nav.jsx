7cbd4f42d1c6649f5f3c4b398bbd78f4a786c5c2
DavidCobb606 <Davidcobb606@gmail.com>
2023-01-24 08:19:02 +0000

    Project set up, some very basic formatting done, and api call set up in very basic form.

---
+++ src/components/Nav.jsx
@@ -0,0 +1,34 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import ExpandMoreOutlinedIcon from "@mui/icons-material/ExpandMoreOutlined";
+import MenuIcon from "@mui/icons-material/Menu";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
+
+const Nav = () => {
+
+    return(
+        <Grid container sx={{ color: "text.primary" }} style={{ margin: "10rem" }}>
+<Grid item xs={1}>
+  <Typography>More</Typography>
+</Grid>
+<Grid item xs={1}>
+  <ExpandMoreOutlinedIcon />
+</Grid>
+<Grid item xs={1}>
+  <Typography>Menu</Typography>
+</Grid>
+<Grid item xs={1}>
+  <MenuIcon />
+</Grid>
+<Grid item xs={1}>
+  <Typography>Cart</Typography>
+</Grid>
+<Grid item xs={1}>
+  <ShoppingCartIcon />
+</Grid>
+</Grid>
+    )
+
+}
+
+export default Nav
